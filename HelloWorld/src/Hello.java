33b4b6894bc8244f4af6fc2e974d6590beb3c8dc
May Wong <maywong@Mays-MacBook-Air.local>
2020-08-26 09:13:27 -0300

    First project - Hello World and start using variables

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,21 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // Hello World section
+        System.out.println("Hello World!");
+        System.out.println("Hello, May!");
+
+        // Variables section
+        int myFirstNumber = 1;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+
+}
